355295281ef0c63ae38a5b6804c0be30251c6ac3
AshenvaleZX <714837923@qq.com>
2022-08-10 21:16:10 +0800

    先临时处理一下场景的初始化，让代码跑起来

--- CPPScripts/Scene.cpp
+++ CPPScripts/Scene.cpp
@@ -4,7 +4,13 @@ namespace ZXEngine
 {
 	void Scene::Render()
 	{
-		for (auto i = 0; i < cameras.size(); ++i) {
+		if (cameras.size() == 0)
+			cameras.push_back(new Camera());
+
+		if (gameObjects.size() == 0)
+			gameObjects.push_back(new GameObject());
+
+		for (unsigned i = 0; i < cameras.size(); ++i) {
 			auto camera = cameras[i];
 			camera->Render(gameObjects);
 		}
--- CPPScripts/SceneManager.cpp
+++ CPPScripts/SceneManager.cpp
@@ -4,6 +4,9 @@ namespace ZXEngine
 {
 	Scene* SceneManager::GetCurScene()
 	{
+		if (curScene == nullptr)
+			curScene = new Scene();
+
 		return curScene;
 	}
 }
