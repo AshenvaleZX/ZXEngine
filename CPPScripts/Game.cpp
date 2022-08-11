1725eed0e6a8aacdddad87409f2f678427e4c899
AshenvaleZX <714837923@qq.com>
2022-08-11 21:57:31 +0800

    改一下窗口名

--- CPPScripts/Game.cpp
+++ CPPScripts/Game.cpp
@@ -28,7 +28,7 @@ namespace ZXEngine
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-		window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+		window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "ZXEngine", NULL, NULL);
 		if (window == NULL)
 		{
 			cout << "Failed to create GLFW window" << std::endl;
