2483b6b23ac08cb7643506e93b5c89d24124007e
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2022-08-15 22:51:34 +0800

    添加日志等级

--- CPPScripts/Debug.h
+++ CPPScripts/Debug.h
@@ -5,6 +5,16 @@ class Debug
 public:
 	static void Log(string message)
 	{
-		cout << message << endl;
+		cout << "Log: " << message << endl;
+	}
+
+	static void LogWarning(string message)
+	{
+		cout << "Warning: " << message << endl;
+	}
+
+	static void LogError(string message)
+	{
+		cout << "Error: " << message << endl;
 	}
 };
