810b260357899d908e674b673564351e545b96a3
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2022-08-21 20:50:42 +0800

    默认参数写到函数声明里

--- CPPScripts/ZCamera.cpp
+++ CPPScripts/ZCamera.cpp
@@ -2,14 +2,7 @@
 
 namespace ZXEngine
 {
-	// 一些相机参数的默认值
-	const float YAW = -90.0f;
-	const float PITCH = 0.0f;
-	const float SPEED = 2.5f;
-	const float SENSITIVITY = 0.1f;
-	const float FOV = 45.0f;
-
-	Camera::Camera(vec3 position = vec3(0.0f, 0.0f, 0.0f), vec3 up = vec3(0.0f, 1.0f, 0.0f), float yaw = YAW, float pitch = PITCH) : Front(vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Fov(FOV)
+	Camera::Camera(vec3 position, vec3 up, float yaw, float pitch) : Front(vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Fov(FOV)
 	{
 		Position = position;
 		WorldUp = up;
