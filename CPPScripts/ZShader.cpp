e267f790aeecf30f90c66d597e9b49c06e94a84c
AshenvaleZX <714837923@qq.com>
2022-10-12 21:23:22 +0800

    Shader增加参数设置接口

--- CPPScripts/ZShader.cpp
+++ CPPScripts/ZShader.cpp
@@ -11,4 +11,70 @@ namespace ZXEngine
 	{
 		return ID;
 	}
+
+	void Shader::Use()
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->UseShader(this->ID);
+	}
+	void Shader::SetBool(string name, bool value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderBool(this->ID, name, value);
+	}
+	void Shader::SetInt(string name, int value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderInt(this->ID, name, value);
+	}
+	void Shader::SetFloat(string name, float value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderFloat(this->ID, name, value);
+	}
+	void Shader::SetVec2(string name, vec2 value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderVec2(this->ID, name, value);
+	}
+	void Shader::SetVec2(string name, float x, float y)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderVec2(this->ID, name, x, y);
+	}
+	void Shader::SetVec3(string name, vec3 value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderVec3(this->ID, name, value);
+	}
+	void Shader::SetVec3(string name, float x, float y, float z)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderVec3(this->ID, name, x, y, z);
+	}
+	void Shader::SetVec4(string name, vec4 value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderVec4(this->ID, name, value);
+	}
+	void Shader::SetVec4(string name, float x, float y, float z, float w)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderVec4(this->ID, name, x, y, z, w);
+	}
+	void Shader::SetMat2(string name, mat2 value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderMat2(this->ID, name, value);
+	}
+	void Shader::SetMat3(string name, mat3 value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderMat3(this->ID, name, value);
+	}
+	void Shader::SetMat4(string name, mat4 value)
+	{
+		auto api = RenderEngine::mInstance->GetRenderAPI();
+		api->SetShaderMat4(this->ID, name, value);
+	}
 }
--- CPPScripts/ZShader.h
+++ CPPScripts/ZShader.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "RenderEngine.h"
+#include "RenderAPI.h"
 
 namespace ZXEngine
 {
@@ -10,6 +11,19 @@ namespace ZXEngine
 		~Shader() {};
 
 		unsigned int GetID();
+		void Use();
+		void SetBool(string name, bool value);
+		void SetInt(string name, int value);
+		void SetFloat(string name, float value);
+		void SetVec2(string name, vec2 value);
+		void SetVec2(string name, float x, float y);
+		void SetVec3(string name, vec3 value);
+		void SetVec3(string name, float x, float y, float z);
+		void SetVec4(string name, vec4 value);
+		void SetVec4(string name, float x, float y, float z, float w);
+		void SetMat2(string name, mat2 value);
+		void SetMat3(string name, mat3 value);
+		void SetMat4(string name, mat4 value);
 
 	private:
 		unsigned int ID;
