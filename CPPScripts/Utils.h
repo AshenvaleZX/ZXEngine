d74c36f99d29fd9aa21449e0222f7ac5281cc668
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2022-11-05 17:53:50 +0800

    Utils加个向量转字符串工具

--- CPPScripts/Utils.h
+++ CPPScripts/Utils.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <glm/glm.hpp>
 
 namespace ZXEngine
 {
@@ -8,5 +9,8 @@ namespace ZXEngine
 	{
 	public:
 		static std::vector<std::string> StringSplit(std::string oriStr, char p);
+		static std::string Vec2ToString(glm::vec2 v);
+		static std::string Vec3ToString(glm::vec3 v);
+		static std::string Vec4ToString(glm::vec4 v);
 	};
 }
