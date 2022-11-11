fc9cc1935e8cf947c757b309824502e8970e6264
AshenvaleZX <714837923@qq.com>
2022-11-11 19:17:26 +0800

    改一下Lua源码的main函数名字，否则和项目的main冲突了

--- CPPScripts/Lua/lua.c
+++ CPPScripts/Lua/lua.c
@@ -647,7 +647,7 @@ static int pmain (lua_State *L) {
 }
 
 
-int main (int argc, char **argv) {
+int main_lua (int argc, char **argv) {
   int status, result;
   lua_State *L = luaL_newstate();  /* create state */
   if (L == NULL) {
