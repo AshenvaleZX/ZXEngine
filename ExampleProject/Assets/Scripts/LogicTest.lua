9f8047030d61c0a370258f07b89c395bcbfdfafc
AshenvaleZX <714837923@qq.com>
2022-11-16 20:02:40 +0800

    在Lua代码里让GameObject移动和旋转起来

--- ExampleProject/Assets/Scripts/LogicTest.lua
+++ ExampleProject/Assets/Scripts/LogicTest.lua
@@ -1,11 +1,24 @@
 local LogicTest = NewGameLogic()
 
+LogicTest.radius = 5
+LogicTest.angle = 0
+LogicTest.rot = 0
+
 function LogicTest:Start()
-    fprint("Start in lua %s", self._ID)
+    self.trans = self.GameLogic:GetTransform()
 end
 
 function LogicTest:Update()
-    fprint("Update in lua %s", self._ID)
+    self.angle = self.angle + 0.5 * Time.GetDeltaTime()
+    local x = math.sin(self.angle)*self.radius
+    local z = math.cos(self.angle)*self.radius
+    self.trans:SetPosition(x, 0, z)
+
+    self.rot = self.rot + 5 * Time.GetDeltaTime()
+    if self.rot > 360 then
+        self.rot = self.rot - 360
+    end
+    self.trans:SetEulerAngles(0, 0, self.rot)
 end
 
 return LogicTest
