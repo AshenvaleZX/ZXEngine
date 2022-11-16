8177024dd15928cd382bae644acf93f13d5b075a
AshenvaleZX <714837923@qq.com>
2022-11-16 19:57:37 +0800

    修改游戏启动第一帧获取deltaTime的逻辑

--- CPPScripts/Time.cpp
+++ CPPScripts/Time.cpp
@@ -13,8 +13,12 @@ namespace ZXEngine
 		long long time_milli = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 		long long time_micro = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
 
-		// 这里用微秒计算deltaTime，用毫秒有可能精度不够
-		deltaTime = (time_micro - curTime_micro) / 100000.0f;
+		// 如果是第一次Update，保持deltaTime为0不变，否则会变成时间戳
+		if (curTime_micro != 0)
+		{
+			// 这里用微秒计算deltaTime，用毫秒有可能精度不够
+			deltaTime = (time_micro - curTime_micro) / 100000.0f;
+		}
 
 		curTime = time;
 		curTime_milli = time_milli;
