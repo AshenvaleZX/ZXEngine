7b0d4c1eee4a409732a79b5906524c3c777e1c5a
AshenvaleZX <714837923@qq.com>
2022-11-22 19:09:59 +0800

    把获取ViewMatrix的接口添加到工具类里

--- CPPScripts/Utils.cpp
+++ CPPScripts/Utils.cpp
@@ -37,4 +37,30 @@ namespace ZXEngine
     {
         return "x " + std::to_string(v.x) + " y " + std::to_string(v.y) + " z " + std::to_string(v.z) + " w " + std::to_string(v.w);
     }
+
+    // 这个函数和GLM的LookAt函数效果是一样的
+    glm::mat4 Utils::GetLookToMatrix(glm::vec3 pos, glm::vec3 forward, glm::vec3 up)
+    {
+        // 学Unity用的左手坐标系，up叉乘forward得到right，右手坐标系得反过来
+        glm::vec3 right = glm::cross(up, forward);
+
+        // 由于GLM里是以列为主序的，平时书面上的矩阵都是以行为主序的，所以这里看起来像是转置过了一样，其实这个viewMat看起来应该是这样的：
+        //  Right.x,  Right.y,  Right.z, 0,
+        //  Up.x,     Up.y,     Up.z,    0,
+        // -Front.x, -Front.y, -Front.z, 0,
+        //  0,        0,        0,       1
+        // 后面的posMat同理
+        glm::mat4 viewMat = glm::mat4(
+            right.x, up.x, -forward.x, 0,
+            right.y, up.y, -forward.y, 0,
+            right.z, up.z, -forward.z, 0,
+            0, 0, 0, 1);
+        glm::mat4 posMat = glm::mat4(
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            -pos.x, -pos.y, -pos.z, 1);
+
+        return viewMat * posMat;
+    }
 }
--- CPPScripts/ZCamera.cpp
+++ CPPScripts/ZCamera.cpp
@@ -28,31 +28,10 @@ namespace ZXEngine
 	// Returns the view matrix calculated using Euler Angles and the LookAt Matrix
 	mat4 Camera::GetViewMatrix()
 	{
-		// return glm::lookAt(Position, Position + Front, Up);
-
-		// 以下尝试自己实现LookAt函数
-		// 由于GLM里是以列为主序的，平时书面上的矩阵都是以行为主序的，所以这里看起来像是转置过了一样，其实这个viewMat看起来应该是这样的：
-		//  Right.x,  Right.y,  Right.z, 0,
-		//  Up.x,     Up.y,     Up.z,    0,
-		// -Front.x, -Front.y, -Front.z, 0,
-		//  0,        0,        0,       1
-		// 后面的posMat同理
 		vec3 pos = GetTransform()->position;
 		vec3 forward = GetTransform()->GetForward();
-		vec3 right = GetTransform()->GetRight();
 		vec3 up = GetTransform()->GetUp();
-		mat4 viewMat = mat4(
-			right.x, up.x, -forward.x, 0,
-			right.y, up.y, -forward.y, 0,
-			right.z, up.z, -forward.z, 0,
-			0, 0, 0, 1);
-		mat4 posMat = mat4(
-			1, 0, 0, 0,
-			0, 1, 0, 0,
-			0, 0, 1, 0,
-			-pos.x, -pos.y, -pos.z, 1);
-
-		return viewMat * posMat;
+		return Utils::GetLookToMatrix(pos, forward, up);
 	}
 
 	mat4 Camera::GetProjectionMatrix()
