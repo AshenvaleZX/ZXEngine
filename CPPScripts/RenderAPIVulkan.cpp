944d15921db8b8ce013557eab971d71ca801c2dd
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2023-03-22 23:41:20 +0800

    Vulkan版本改到1.3

--- CPPScripts/RenderAPIVulkan.cpp
+++ CPPScripts/RenderAPIVulkan.cpp
@@ -1232,10 +1232,10 @@ namespace ZXEngine
         VkApplicationInfo appInfo = {};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "ZXEngineApplication";
-        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.applicationVersion = VK_API_VERSION_1_3;
         appInfo.pEngineName = "ZXEngine";
-        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
+        appInfo.engineVersion = VK_API_VERSION_1_3;
+        appInfo.apiVersion = VK_API_VERSION_1_3;
 
         VkInstanceCreateInfo createInfo = {};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
