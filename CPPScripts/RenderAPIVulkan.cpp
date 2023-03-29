0864ee9f4d69fc7ee80a7acdefbaf63cc910256d
AshenvaleZX <714837923@qq.com>
2023-03-29 13:20:22 +0800

    Vulkan文本纹理创建完成后忘了转换Layout

--- CPPScripts/RenderAPIVulkan.cpp
+++ CPPScripts/RenderAPIVulkan.cpp
@@ -376,6 +376,12 @@ namespace ZXEngine
 
         DestroyBuffer(stagingBuffer);
 
+        TransitionImageLayout(image.image,
+            VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL,
+            VK_IMAGE_ASPECT_COLOR_BIT,
+            VK_PIPELINE_STAGE_TRANSFER_BIT, VK_ACCESS_TRANSFER_WRITE_BIT,
+            VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT, VK_ACCESS_SHADER_READ_BIT);
+
         VkImageView imageView = CreateImageView(image.image, VK_FORMAT_R16_SFLOAT, VK_IMAGE_ASPECT_COLOR_BIT, VK_IMAGE_VIEW_TYPE_2D);
         VkSampler sampler = CreateSampler(1);
 
