9ef838ac52020490bbb6bab2059c3516c5e253df
AshenvaleZX <714837923@qq.com>
2023-04-18 22:00:43 +0800

    添加对Windows窗口大小变化的响应处理

--- CPPScripts/Window/WindowManagerWindows.cpp
+++ CPPScripts/Window/WindowManagerWindows.cpp
@@ -1,11 +1,15 @@
 #include "WindowManagerWindows.h"
 #include <WindowsX.h>
+#include "../RenderAPI.h"
 #include "../ProjectSetting.h"
 #include "../Input/InputManager.h"
 
 namespace ZXEngine
 {
-	LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	{
+		return static_cast<WindowManagerWindows*>(WindowManager::GetInstance())->WindowProcedure(hWnd, msg, wParam, lParam);
+	}
 
 	WindowManagerWindows::WindowManagerWindows()
 	{
@@ -25,6 +29,8 @@ namespace ZXEngine
 
 		RegisterClassExW(&wndClass);
 
+		mWindowWidth = ProjectSetting::srcWidth;
+		mWindowHeight = ProjectSetting::srcHeight;
 		mWindow = CreateWindowW(wndClass.lpszClassName, L"ZXEngine", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
 			ProjectSetting::srcWidth, ProjectSetting::srcHeight, NULL, NULL, wndClass.hInstance, NULL);
 
@@ -47,15 +53,85 @@ namespace ZXEngine
 		return false;
 	}
 
-	LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+	LRESULT WindowManagerWindows::WindowProcedure(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	{
 		switch (msg)
 		{
+		// 窗口大小变化
+		case WM_SIZE:
+			mWindowWidth = static_cast<uint32_t>(LOWORD(lParam));
+			mWindowHeight = static_cast<uint32_t>(HIWORD(lParam));
+
+			// 窗口最小化
+			if (wParam == SIZE_MINIMIZED)
+			{
+				mAppPaused = true;
+				mMinimized = true;
+				mMaximized = false;
+			}
+			// 窗口最大化
+			else if (wParam == SIZE_MAXIMIZED)
+			{
+				mAppPaused = false;
+				mMinimized = false;
+				mMaximized = true;
+				OnResize();
+			}
+			// 窗口大小恢复
+			else if (wParam == SIZE_RESTORED)
+			{
+				// 从最小化恢复
+				if (mMinimized)
+				{
+					mAppPaused = false;
+					mMinimized = false;
+					OnResize();
+				}
+				// 从最大化恢复
+				else if (mMaximized)
+				{
+					mAppPaused = false;
+					mMaximized = false;
+					OnResize();
+				}
+				// 用户正在拖动窗口边框
+				else if (mResizing)
+				{
+					// 此时窗口大小会一直不停变化，不做响应，等拖完了再处理
+					// 也就是收到 WM_EXITSIZEMOVE 的时候
+				}
+				// 一些其他情况导致的窗口变化，比如 SetWindowPos 或 mSwapChain->SetFullscreenState 等接口的调用
+				else
+				{
+					OnResize();
+				}
+			}
+			return 0;
+
+		// 用户在拖窗口边框
+		case WM_ENTERSIZEMOVE:
+			mAppPaused = true;
+			mResizing = true;
+			return 0;
+
+		// 用户松开了窗口边框
+		case WM_EXITSIZEMOVE:
+			mAppPaused = false;
+			mResizing = false;
+			OnResize();
+			return 0;
+
+		// 鼠标移动
 		case WM_MOUSEMOVE:
 			InputManager::GetInstance()->UpdateMousePos(GET_X_LPARAM(lParam), GET_Y_LPARAM(lParam));
 			return 0;
 		}
 
 		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+
+	void WindowManagerWindows::OnResize()
+	{
+		RenderAPI::GetInstance()->OnWindowSizeChange(mWindowWidth, mWindowHeight);
 	}
 }
--- CPPScripts/Window/WindowManagerWindows.h
+++ CPPScripts/Window/WindowManagerWindows.h
@@ -13,7 +13,20 @@ namespace ZXEngine
 		virtual void CloseWindow(string args);
 		virtual bool WindowShouldClose();
 
+	public:
+		LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 	private:
 		HWND mWindow;
+
+		uint32_t mWindowWidth;
+		uint32_t mWindowHeight;
+
+		bool mResizing = false;
+		bool mMinimized = false;
+		bool mMaximized = false;
+		bool mAppPaused = false;
+
+		void OnResize();
 	};
 }
