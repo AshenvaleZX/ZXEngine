00ef3d749d7981a7048ff01716b58cc5c2c2d421
AshenvaleZX <714837923@qq.com>
2023-05-30 19:44:22 +0800

    调整RenderPassRayTracing，沿用FBO切换机制

--- CPPScripts/RenderPassRayTracing.cpp
+++ CPPScripts/RenderPassRayTracing.cpp
@@ -6,6 +6,8 @@
 #include "MaterialData.h"
 #include "ZCamera.h"
 #include "Light.h"
+#include "FBOManager.h"
+#include "GlobalData.h"
 
 namespace ZXEngine
 {
@@ -25,6 +27,12 @@ namespace ZXEngine
 		auto renderAPI = RenderAPI::GetInstance();
 		auto renderQueue = RenderQueueManager::GetInstance()->GetRenderQueue((int)RenderQueueType::Qpaque);
 
+		// 切换到主FBO
+		FBOManager::GetInstance()->SwitchFBO("Main");
+		// ViewPort设置为窗口大小
+		renderAPI->SetViewPort(GlobalData::srcWidth, GlobalData::srcHeight);
+
+		// 遍历渲染队列，将所有mesh的VAO和材质数据推入光追管线
 		for (auto renderer : renderQueue->GetRenderers())
 		{
 			renderAPI->PushRayTracingMaterialData(renderer->matetrial->data);
@@ -34,13 +42,19 @@ namespace ZXEngine
 			}
 		}
 
+		// 构建TLAS
 		renderAPI->BuildTopLevelAccelerationStructure(asCommandID);
 
+		// 光追管线常量数据
 		rtConstants.VP = camera->GetProjectionMatrix() * camera->GetViewMatrix();
 		rtConstants.V_Inv = camera->GetViewMatrixInverse();
 		rtConstants.P_Inv = camera->GetProjectionMatrixInverse();
 		rtConstants.lightPos = Light::GetAllLights()[0]->GetTransform()->GetPosition();
 
+		// 光追渲染
 		renderAPI->RayTrace(rtCommandID, rtConstants);
+
+		// 清理渲染队列
+		RenderQueueManager::GetInstance()->ClearAllRenderQueue();
 	}
 }
