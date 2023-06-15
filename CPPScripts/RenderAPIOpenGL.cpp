27354991ffe348237a8a449087cdefbd24f2f441
AshenvaleZX <714837923@qq.com>
2023-06-15 21:56:31 +0800

    处理OpenGL加载CubeMap时的格式问题

--- CPPScripts/RenderAPIOpenGL.cpp
+++ CPPScripts/RenderAPIOpenGL.cpp
@@ -276,7 +276,15 @@ namespace ZXEngine
 			unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrComponents, 0);
 			if (data)
 			{
-				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+				GLenum format = GL_RGB;
+				if (nrComponents == 1)
+					format = GL_RED;
+				else if (nrComponents == 3)
+					format = GL_RGB;
+				else if (nrComponents == 4)
+					format = GL_RGBA;
+
+				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
 				stbi_image_free(data);
 			}
 			else
