ee2c409d5618d9e6aa2965679019d104b6a89b3b
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2024-03-08 18:43:41 +0800

    xmake build with some checks.

---
+++ xmake.lua
@@ -0,0 +1,57 @@
+set_xmakever("2.8.7")
+add_rules("mode.release", "mode.debug")
+if is_mode("debug") then
+    set_targetdir("bin/debug")
+else
+    set_targetdir("bin/release")
+end
+
+includes("xmake/xmake_func.lua")
+
+target("vulkan-sdk")
+set_kind("phony")
+on_load(function(target)
+    local vk_path = os.getenv("VULKAN_SDK")
+    if not vk_path then
+        vk_path = os.getenv("VK_SDK_PATH")
+    end
+    if is_plat("linux", "macosx") then
+        target:add("linkdirs", path.join(vk_path, "lib"), {
+            public = true
+        })
+        target:add("links", "vulkan", {
+            public = true
+        })
+        target:add("includedirs", path.join(vk_path, "include"), {
+            public = true
+        })
+    else
+        target:add("linkdirs", path.join(vk_path, "Lib"), {
+            public = true
+        })
+        target:add("links", "vulkan-1", {
+            public = true
+        })
+        target:add("includedirs", path.join(vk_path, "Include"), {
+            public = true
+        })
+    end
+end)
+target_end()
+
+target("zxengine")
+set_kind("binary")
+add_files("CPPScripts/**.cpp", "CPPScripts/**.c", "Vendor/Src/**.c")
+add_includedirs("Vendor/Include")
+add_rules("basic_settings", "check-winsdk")
+add_deps("vulkan-sdk")
+add_linkdirs(path.join(os.projectdir(), "Vendor/Libs"))
+add_links("assimp-vc143-mt", "freetype", "glfw3", "irrKlang", "opengl32", "Shell32")
+add_defines("UNICODE")
+-- copy dll
+after_build(function(target)
+    local bin_dir = path.join(os.projectdir(), "bin", get_config("mode"))
+    local src_dir = path.join(os.projectdir(), "ZXEngine/Build")
+    os.cp(path.join(src_dir, "*.dll"), bin_dir)
+end)
+target_end()
---
+++ xmake/xmake_func.lua
@@ -0,0 +1,152 @@
+rule("basic_settings")
+on_config(function(target)
+    local _, cc = target:tool("cxx")
+    if is_plat("linux") then
+        -- Linux should use -stdlib=libc++
+        -- https://github.com/LuisaGroup/LuisaCompute/issues/58
+        if (cc == "clang" or cc == "clangxx") then
+            target:add("cxflags", "-stdlib=libc++", {
+                force = true
+            })
+            target:add("syslinks", "c++")
+        end
+    end
+    -- disable LTO
+    -- if cc == "cl" then
+    --     target:add("cxflags", "-GL")
+    -- elseif cc == "clang" or cc == "clangxx" then
+    --     target:add("cxflags", "-flto=thin")
+    -- elseif cc == "gcc" or cc == "gxx" then
+    --     target:add("cxflags", "-flto")
+    -- end
+    -- local _, ld = target:tool("ld")
+    -- if ld == "link" then
+    --     target:add("ldflags", "-LTCG")
+    --     target:add("shflags", "-LTCG")
+    -- elseif ld == "clang" or ld == "clangxx" then
+    --     target:add("ldflags", "-flto=thin")
+    --     target:add("shflags", "-flto=thin")
+    -- elseif ld == "gcc" or ld == "gxx" then
+    --     target:add("ldflags", "-flto")
+    --     target:add("shflags", "-flto")
+    -- end
+end)
+on_load(function(target)
+    local _get_or = function(name, default_value)
+        local v = target:values(name)
+        if v == nil then
+            return default_value
+        end
+        return v
+    end
+    if not is_plat("windows") then
+        if project_kind == "static" then
+            target:add("cxflags", "-fPIC", {
+                tools = {"clang", "gcc"}
+            })
+        end
+    end
+    -- fma support
+    if is_arch("x64", "x86_64") then
+        target:add("cxflags", "-mfma", {
+            tools = {"clang", "gcc"}
+        })
+    end
+    local c_standard = target:values("c_standard")
+    local cxx_standard = target:values("cxx_standard")
+    if type(c_standard) == "string" and type(cxx_standard) == "string" then
+        target:set("languages", c_standard, cxx_standard)
+    else
+        target:set("languages", "clatest", "cxx20")
+    end
+
+    local enable_exception = _get_or("enable_exception", nil)
+    if enable_exception then
+        target:set("exceptions", "cxx")
+    else
+        target:set("exceptions", "no-cxx")
+    end
+
+    if is_mode("debug") then
+        target:set("runtimes", "MDd")
+        target:set("optimize", "none")
+        target:set("warnings", "none")
+        target:add("cxflags", "/GS", "/Gd", {
+            tools = {"clang_cl", "cl"}
+        })
+    elseif is_mode("releasedbg") then
+        target:set("runtimes", "MD")
+        target:set("optimize", "none")
+        target:set("warnings", "none")
+        target:add("cxflags", "/GS-", "/Gd", {
+            tools = {"clang_cl", "cl"}
+        })
+    else
+        target:set("runtimes", "MD")
+        target:set("optimize", "aggressive")
+        target:set("warnings", "none")
+        target:add("cxflags", "/GS-", "/Gd", {
+            tools = {"clang_cl", "cl"}
+        })
+    end
+    target:add("cxflags", "/Zc:preprocessor", {
+        tools = "cl"
+    });
+    if _get_or("use_simd", true) then
+        if is_arch("arm64") then
+            target:add("vectorexts", "neon")
+        else
+            target:add("vectorexts", "avx", "avx2")
+        end
+    end
+    if _get_or("no_rtti", false) then
+        target:add("cxflags", "/GR-", {
+            tools = {"clang_cl", "cl"}
+        })
+        target:add("cxflags", "-fno-rtti", "-fno-rtti-data", {
+            tools = {"clang"}
+        })
+        target:add("cxflags", "-fno-rtti", {
+            tools = {"gcc"}
+        })
+    end
+end)
+rule_end()
+
+rule("check-winsdk")
+on_config(function(target)
+    if not is_plat("windows") then
+        return
+    end
+    local toolchain = get_config("toolchain")
+    if not toolchain then
+        if is_plat("windows") then
+            toolchain = "msvc"
+        elseif is_plat("linux")  then
+            toolchain = "gcc"
+        else
+            toolchain = "clang"
+        end
+    end
+    local sdk_version = target:toolchain(toolchain):runenvs().WindowsSDKVersion
+    
+    local legal_sdk = false
+    if sdk_version then
+        local lib = import("lib")
+        local vers = lib.string_split(sdk_version, '.')
+        if #vers > 0 then
+            if tonumber(vers[1]) > 10 then
+                legal_sdk = true
+            elseif tonumber(vers[1]) == 10 then
+                if #vers > 2 then
+                    if tonumber(vers[3]) >= 22000 then
+                        legal_sdk = true
+                    end
+                end
+            end
+        end
+    end
+    if not legal_sdk then
+        os.raise("Illegal windows SDK version, requires 10.0.22000.0 or later")
+    end
+end)
