fc06731a9b7ce1ff54ba10fc1035bef1664761a4
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2024-05-16 21:51:46 +0800

    将Prefab的加载和销毁接口Wrap到Lua

--- CPPScripts/LuaWrap/Lua_Resources.h
+++ CPPScripts/LuaWrap/Lua_Resources.h
@@ -14,9 +14,33 @@ static int Resources_GetAssetsPath(lua_State* L)
 	return 1;
 }
 
+static int Resources_LoadPrefab(lua_State* L)
+{
+	string path = lua_tostring(L, -1);
+	ZXEngine::PrefabStruct* prefab = ZXEngine::Resources::LoadPrefab(path);
+
+	if (prefab == nullptr)
+	{
+		lua_pushnil(L);
+		return 1;
+	}
+
+	lua_pushlightuserdata(L, prefab);
+	return 1;
+}
+
+static int Resources_ReleasePrefab(lua_State* L)
+{
+	ZXEngine::PrefabStruct* prefab = (ZXEngine::PrefabStruct*)lua_touserdata(L, -1);
+	delete prefab;
+	return 0;
+}
+
 static const luaL_Reg Resources_Funcs[] = 
 {
 	{ "GetAssetsPath", Resources_GetAssetsPath },
+	{ "LoadPrefab",    Resources_LoadPrefab    },
+	{ "ReleasePrefab", Resources_ReleasePrefab },
 	{ NULL, NULL }
 };
 
