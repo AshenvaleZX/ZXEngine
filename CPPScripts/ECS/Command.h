202f5cc458ff4959ff70022fcbe50fd8a1b1057f
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2024-06-19 22:07:58 +0800

    Command添加默认构造函数，将模板函数的实现移出类的声明增强接口的阅读性

--- CPPScripts/ECS/Command.h
+++ CPPScripts/ECS/Command.h
@@ -9,71 +9,28 @@ namespace ZXEngine
 		class Command
 		{
 		public:
-			Command() = delete;
+			Command() : mWorld(World::GetReference()) {}
 			Command(World& world) : mWorld(world) {}
 
 			void Execute();
 
 			template<typename... ComponentTypes>
-			Command& SpawnEntity(ComponentTypes&&... components)
-			{
-				Entity entity = EntityGenerator::Gen();
-				DoSpawnEntity(entity, std::forward<ComponentTypes>(components)...);
-				return *this;
-			}
+			Command& SpawnEntity(ComponentTypes&&... components);
 
 			template<typename... ComponentTypes>
-			Command& SpawnEntityD(ComponentTypes&&... components)
-			{
-				EntitySpawner entitySpawner;
-				entitySpawner.entity = EntityGenerator::Gen();
-				DoSpawnEntityD(entitySpawner.entity, entitySpawner.componentSpawners, std::forward<ComponentTypes>(components)...);
-				return *this;
-			}
+			Command& SpawnEntityD(ComponentTypes&&... components);
 
 			Command& DestroyEntity(Entity entity);
 			Command& DestroyEntityD(Entity entity);
 
 			template<typename T>
-			Command& SpawnSingleton(T&& singleton)
-			{
-				auto singletonID = TypeIDAllocator<World::Singleton>::Get<T>();
-				
-				if (auto iter = mWorld.mSingletons.find(singletonID); iter == mWorld.mSingletons.end())
-				{
-					auto newSingleton = mWorld.mSingletons.emplace(singletonID, World::Singleton(
-						[](void* ptr)->void { delete (T*)ptr; }
-					));
-					newSingleton.first->second.mInstance = new T();
-					*(static_cast<T*>(newSingleton.first->second.mInstance)) = std::forward<T>(singleton);
-				}
-				else
-				{
-					Debug::LogError("Try to spawn an existing singleton.");
-				}
-
-				return *this;
-			}
+			Command& SpawnSingleton(T&& singleton);
 
 			template<typename T>
-			Command& DestroySingleton()
-			{
-				auto singletonID = TypeIDAllocator<World::Singleton>::Get<T>();
-
-				DoDestroySingleton(singletonID);
-
-				return *this;
-			}
+			Command& DestroySingleton();
 			
 			template<typename T>
-			Command& DestroySingletonD()
-			{
-				auto singletonID = TypeIDAllocator<World::Singleton>::Get<T>();
-
-				mSingletonsToDestroy.push_back(singletonID);
-
-				return *this;
-			}
+			Command& DestroySingletonD();
 
 		private:
 			World& mWorld;
@@ -100,67 +57,131 @@ namespace ZXEngine
 			vector<EntitySpawner> mEntitiesToSpawn;
 
 			template<typename T, typename... Components>
-			void DoSpawnEntity(Entity entity, T&& component, Components&&... components)
-			{
-				auto componentID = TypeIDAllocator<World::ComponentPool>::Get<T>();
-
-				if (auto iter = mWorld.mComponents.find(componentID); iter == mWorld.mComponents.end())
-				{
-					mWorld.mComponents.emplace(componentID, World::ComponentData(
-						[]()->void* { return new T(); },
-						[](void* ptr)->void { delete (T*)ptr; }
-					));
-				}
-
-				auto& componentData = mWorld.mComponents[componentID];
-				void* newComponent = componentData.mPool.Create();
-
-				*(static_cast<T*>(newComponent)) = std::forward<T>(component);
-				componentData.mEntitySet.Add(entity);
-
-				auto iter = mWorld.mEntities.emplace(entity, ComponentContainer{});
-				iter.first->second[componentID] = newComponent;
-
-				if constexpr (sizeof...(components) > 0)
-				{
-					DoSpawnEntity<Components...>(entity, std::forward<Components>(components)...);
-				}
-			}
+			void DoSpawnEntity(Entity entity, T&& component, Components&&... components);
 
 			template<typename T, typename... Components>
-			void DoSpawnEntityD(Entity entity, vector<ComponentSpawner>& spawners, T&& component, Components&&... components)
-			{
-				ComponentSpawner spawner;
-				spawner.ID = TypeIDAllocator<World::ComponentPool>::Get<T>();
-
-				// 这里如果component是以左值传入的，会产生复制开销，所以在调用时尽可能的使用右值传入
-				spawner.Assign = [capturedComponent = std::forward<T>(component)](void* ptr)mutable->void
-				{
-					*(static_cast<T*>(ptr)) = std::move(capturedComponent);
-				};
-
-				spawner.Create = [](void)->void*
-				{
-					return new T();
-				};
-
-				spawner.Destroy = [](void* ptr)->void
-				{
-					delete static_cast<T*>(ptr);
-				};
-
-				spawners.push_back(spawner);
-
-				if constexpr (sizeof...(components) > 0)
-				{
-					DoSpawnEntityD<Components...>(entity, spawners, std::forward<Components>(components)...);
-				}
-			}
+			void DoSpawnEntityD(Entity entity, vector<ComponentSpawner>& spawners, T&& component, Components&&... components);
 
 			void RealDoSpawnEntityD();
 			void* SpawnComponentForEntity(Entity entity, const ComponentSpawner& spawner);
 
 			void DoDestroySingleton(uint32_t id);
 		};
+
+		template<typename... ComponentTypes>
+		Command& Command::SpawnEntity(ComponentTypes&&... components)
+		{
+			Entity entity = EntityGenerator::Gen();
+			DoSpawnEntity(entity, std::forward<ComponentTypes>(components)...);
+			return *this;
+		}
+
+		template<typename... ComponentTypes>
+		Command& Command::SpawnEntityD(ComponentTypes&&... components)
+		{
+			EntitySpawner entitySpawner;
+			entitySpawner.entity = EntityGenerator::Gen();
+			DoSpawnEntityD(entitySpawner.entity, entitySpawner.componentSpawners, std::forward<ComponentTypes>(components)...);
+			return *this;
+		}
+
+		template<typename T>
+		Command& Command::SpawnSingleton(T&& singleton)
+		{
+			auto singletonID = TypeIDAllocator<World::Singleton>::Get<T>();
+
+			if (auto iter = mWorld.mSingletons.find(singletonID); iter == mWorld.mSingletons.end())
+			{
+				auto newSingleton = mWorld.mSingletons.emplace(singletonID, World::Singleton(
+					[](void* ptr)->void { delete (T*)ptr; }
+				));
+				newSingleton.first->second.mInstance = new T();
+				*(static_cast<T*>(newSingleton.first->second.mInstance)) = std::forward<T>(singleton);
+			}
+			else
+			{
+				Debug::LogError("Try to spawn an existing singleton.");
+			}
+
+			return *this;
+		}
+
+		template<typename T>
+		Command& Command::DestroySingleton()
+		{
+			auto singletonID = TypeIDAllocator<World::Singleton>::Get<T>();
+
+			DoDestroySingleton(singletonID);
+
+			return *this;
+		}
+
+		template<typename T>
+		Command& Command::DestroySingletonD()
+		{
+			auto singletonID = TypeIDAllocator<World::Singleton>::Get<T>();
+
+			mSingletonsToDestroy.push_back(singletonID);
+
+			return *this;
+		}
+
+		template<typename T, typename... Components>
+		void Command::DoSpawnEntity(Entity entity, T&& component, Components&&... components)
+		{
+			auto componentID = TypeIDAllocator<World::ComponentPool>::Get<T>();
+
+			if (auto iter = mWorld.mComponents.find(componentID); iter == mWorld.mComponents.end())
+			{
+				mWorld.mComponents.emplace(componentID, World::ComponentData(
+					[]()->void* { return new T(); },
+					[](void* ptr)->void { delete (T*)ptr; }
+				));
+			}
+
+			auto& componentData = mWorld.mComponents[componentID];
+			void* newComponent = componentData.mPool.Create();
+
+			*(static_cast<T*>(newComponent)) = std::forward<T>(component);
+			componentData.mEntitySet.Add(entity);
+
+			auto iter = mWorld.mEntities.emplace(entity, ComponentContainer{});
+			iter.first->second[componentID] = newComponent;
+
+			if constexpr (sizeof...(components) > 0)
+			{
+				DoSpawnEntity<Components...>(entity, std::forward<Components>(components)...);
+			}
+		}
+
+		template<typename T, typename... Components>
+		void Command::DoSpawnEntityD(Entity entity, vector<ComponentSpawner>& spawners, T&& component, Components&&... components)
+		{
+			ComponentSpawner spawner;
+			spawner.ID = TypeIDAllocator<World::ComponentPool>::Get<T>();
+
+			// 这里如果component是以左值传入的，会产生复制开销，所以在调用时尽可能的使用右值传入
+			spawner.Assign = [capturedComponent = std::forward<T>(component)](void* ptr)mutable->void
+				{
+					*(static_cast<T*>(ptr)) = std::move(capturedComponent);
+				};
+
+			spawner.Create = [](void)->void*
+				{
+					return new T();
+				};
+
+			spawner.Destroy = [](void* ptr)->void
+				{
+					delete static_cast<T*>(ptr);
+				};
+
+			spawners.push_back(spawner);
+
+			if constexpr (sizeof...(components) > 0)
+			{
+				DoSpawnEntityD<Components...>(entity, spawners, std::forward<Components>(components)...);
+			}
+		}
 	}
 }
