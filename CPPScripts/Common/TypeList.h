792d2659c0fa61f2b54b41c074ceba1b4cae67fe
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2024-09-03 21:08:36 +0800

    新增一个TypeList，辅助后续的模板元代码

---
+++ CPPScripts/Common/TypeList.h
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace ZXEngine
+{
+    template <typename... Ts>
+    struct TypeList
+    {
+        using SelfType = TypeList<Ts...>;
+        static constexpr size_t size = sizeof...(Ts);
+    };
+
+    namespace Internal
+    {
+        // 仅声明，未实际使用模板，省略了T，等价于template <typename T>
+        template <typename>
+        struct ListSize;
+
+        // template <typename...> 在 ListType 的定义中确保了 ListType 是一个接受可变数量类型参数的模板
+        // 如果没有这个约束，ListType 可能只是一个普通的类型，而不是一个模板
+        template <template <typename...> typename ListType, typename... Ts>
+        struct ListSize<ListType<Ts...>>
+        {
+            static constexpr size_t value = sizeof...(Ts);
+        };
+    }
+
+    template <typename List>
+    constexpr size_t ListSize_V = Internal::ListSize<List>::value;
+
+    template <typename List>
+    constexpr bool IsListEmpty_V = ListSize_V<List> == 0;
+}
