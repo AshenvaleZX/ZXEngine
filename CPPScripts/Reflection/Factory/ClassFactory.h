39068b6bdd75b8db55cf345c3df77a6c73219317
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2024-09-25 22:21:30 +0800

    调整ClassFactory接口

--- CPPScripts/Reflection/Factory/ClassFactory.h
+++ CPPScripts/Reflection/Factory/ClassFactory.h
@@ -25,19 +25,25 @@ namespace ZXEngine
 				return instance;
 			}
 
+		public:
+			const ClassType& GetTypeInfo() const
+			{
+				return mTypeInfo;
+			}
+
 			void Register(const std::string& name)
 			{
 				mTypeInfo.mName = name;
 			}
 
 			template <typename T1>
-			void AddVariable(const string& name)
+			void AddVariable(const string& name, T1 type)
 			{
 				mTypeInfo.AddVariable(MemberVariable::Create<T1>(name));
 			}
 
 			template <typename T1>
-			void AddFunction(const string& name)
+			void AddFunction(const string& name, T1 type)
 			{
 				mTypeInfo.AddFunction(MemberFunction::Create<T1>(name));
 			}
