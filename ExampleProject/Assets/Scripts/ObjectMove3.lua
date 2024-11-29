5e55c93d052b0c815423fba75d02220772d5d852
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2024-11-29 21:05:06 +0800

    调整演示场景里的粒子系统

---
+++ ExampleProject/Assets/Scripts/ObjectMove3.lua
@@ -0,0 +1,19 @@
+local ObjectMove2 = NewGameLogic()
+
+ObjectMove2.radius = 20
+ObjectMove2.angle = 0
+ObjectMove2.rot = 0
+
+function ObjectMove2:Start()
+    self.trans = self.gameObject:GetComponent("Transform")
+end
+
+function ObjectMove2:Update()
+    self.angle = self.angle - 1 * Time.GetDeltaTime()
+    local x = math.sin(self.angle)*self.radius
+    local z = math.cos(self.angle)*self.radius
+    self.trans:SetPosition(x, 10, z)
+    self.trans:SetEulerAngles(0, math.deg(self.angle), 0)
+end
+
+return ObjectMove2
