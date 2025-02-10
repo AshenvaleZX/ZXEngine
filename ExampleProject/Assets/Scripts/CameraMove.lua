57aed244bde9267720ac456b23aa5572e956a92a
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2025-02-10 12:59:55 +0800

    演示Demo的相机添加触屏操控支持

--- ExampleProject/Assets/Scripts/CameraMove.lua
+++ ExampleProject/Assets/Scripts/CameraMove.lua
@@ -4,6 +4,9 @@ CameraMove.firstMouse = true
 CameraMove.MouseSensitivity = 0.1
 CameraMove.MovementSpeed = 25
 
+CameraMove.LastTouchDis = 0
+CameraMove.IsFirstTouchZoom = true
+
 function CameraMove:Start()
     self.trans = self.gameObject:GetComponent("Transform")
     EngineEvent:AddEventHandler(EngineEventType.MOUSE_BUTTON_2_DOWN, self.RegisterCameraControl, self)
@@ -11,7 +14,7 @@ function CameraMove:Start()
 end
 
 function CameraMove:Update()
-
+    self:UpdateTouch()
 end
 
 function CameraMove:RegisterCameraControl()
@@ -33,10 +36,48 @@ function CameraMove:UnregisterCameraControl()
     EngineEvent:RemoveEventHandler(EngineEventType.KEY_D_PRESS, self.MoveRightCallBack)
 end
 
+function CameraMove:UpdateTouch()
+    local touchCount = InputManager.GetTouchCount()
+    if touchCount == 1 then
+        local touch = InputManager.GetTouch(0)
+        if touch.phase == TouchPhase.Began then
+            self.firstMouse = true
+        elseif touch.phase == TouchPhase.Moved then
+            self:MouseMove(touch.x, touch.y)
+        end
+        self.IsFirstTouchZoom = true
+    elseif touchCount == 2 then
+        local touch1 = InputManager.GetTouch(0)
+        local touch2 = InputManager.GetTouch(1)
+        if self.IsFirstTouchZoom then
+            self.LastTouchDis = math.sqrt((touch1.x - touch2.x) * (touch1.x - touch2.x) + (touch1.y - touch2.y) * (touch1.y - touch2.y))
+            self.IsFirstTouchZoom = false
+        else
+            local touchDis = math.sqrt((touch1.x - touch2.x) * (touch1.x - touch2.x) + (touch1.y - touch2.y) * (touch1.y - touch2.y))
+            local delta = touchDis - self.LastTouchDis
+
+            self.MovementSpeed = 100
+            if delta > 0 then
+                self:MoveCamera("Forward")
+            else
+                self:MoveCamera("Back")
+            end
+        end
+        self.firstMouse = true
+    else
+        self.firstMouse = true
+        self.IsFirstTouchZoom = true
+    end
+end
+
 function CameraMove:MouseMoveCallBack(args)
     local argList = Utils.StringSplit(args, '|')
     local xpos = tonumber(argList[1])
     local ypos = tonumber(argList[2])
+    self:MouseMove(xpos, ypos)
+end
+
+function CameraMove:MouseMove(xpos, ypos)
     if self.firstMouse then
         self.lastX = xpos
         self.lastY = ypos
