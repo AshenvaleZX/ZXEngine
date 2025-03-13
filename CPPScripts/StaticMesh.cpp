75245f3bfea2d1903fe5ba2ae89138dc61ae1af4
AshenvaleZX <39127012+AshenvaleZX@users.noreply.github.com>
2025-03-13 22:26:41 +0800

    StaticMesh添加skinned属性，通过计算管线更新动画时会需要使用

--- CPPScripts/StaticMesh.cpp
+++ CPPScripts/StaticMesh.cpp
@@ -2,26 +2,28 @@
 
 namespace ZXEngine
 {
-	StaticMesh::StaticMesh(vector<Vertex>&& vertices, vector<uint32_t>&& indices, bool setup)
+	StaticMesh::StaticMesh(vector<Vertex>&& vertices, vector<uint32_t>&& indices, bool setup, bool skinned)
 	{
 		mVertices = std::move(vertices);
 		mIndices = std::move(indices);
+		mSkinned = skinned;
 
 		if (setup)
 		{
-			RenderAPI::GetInstance()->SetUpStaticMesh(VAO, mVertices, mIndices);
+			RenderAPI::GetInstance()->SetUpStaticMesh(VAO, mVertices, mIndices, skinned);
 			mIsSetUp = true;
 		}
 	}
 
-	StaticMesh::StaticMesh(const vector<Vertex>& vertices, const vector<uint32_t>& indices, bool setup)
+	StaticMesh::StaticMesh(const vector<Vertex>& vertices, const vector<uint32_t>& indices, bool setup, bool skinned)
 	{
 		mVertices = vertices;
 		mIndices = indices;
+		mSkinned = skinned;
 
 		if (setup)
 		{
-			RenderAPI::GetInstance()->SetUpStaticMesh(VAO, vertices, indices);
+			RenderAPI::GetInstance()->SetUpStaticMesh(VAO, vertices, indices, skinned);
 			mIsSetUp = true;
 		}
 	}
@@ -31,7 +33,7 @@ namespace ZXEngine
 		if (mIsSetUp)
 			return;
 
-		RenderAPI::GetInstance()->SetUpStaticMesh(VAO, mVertices, mIndices);
+		RenderAPI::GetInstance()->SetUpStaticMesh(VAO, mVertices, mIndices, mSkinned);
 		mIsSetUp = true;
 	}
 }
